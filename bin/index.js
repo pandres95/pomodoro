224a4204bdeeaf05ad3dfe7c5971857700238f37
Pablo Andrés Dorado Suárez <im@pandres95.me>
2018-07-27 06:27:44 -0500

    Fixed command running and daemon execution

--- bin/index.js
+++ bin/index.js
@@ -2,5 +2,55 @@
 
 'use strict';
 
-require('daemon')({ cwd: __dirname });
-require('./worker.js');
+const args = [].concat(process.argv);
+args.shift();
+args.shift();
+
+const Promisify = require('es-promisify');
+
+const os = require('os');
+const {
+    constants,
+    access: accessAsync,
+    readFile: readFileAsync,
+    writeFile: writeFileAsync
+} = require('fs');
+
+const timeout = milis =>
+    new Promise((resolve, reject) => setTimeout(resolve, milis));
+const access = Promisify(accessAsync);
+const readFile = Promisify(readFileAsync);
+const writeFile = Promisify(writeFileAsync);
+
+async function main () {
+    let child = require('daemon').daemon('./worker.js', args, { cwd: __dirname });
+
+    if (os.platform() === 'darwin' || os.platform() === 'linux') {
+        let pidPath = '/var/run';
+
+        if (os.platform() === 'darwin') {
+            pidPath = '/usr/local' + pidPath;
+        }
+
+        pidPath = pidPath + '/pomodoro.pid';
+
+        try {
+            try {
+                await access(pidPath, constants.F_OK | constants.W_OK);
+
+                const fileData = await readFile(pidPath);
+                const pid = fileData.toString();
+                process.kill(pid);
+            } catch (error) {}
+
+            await writeFile(pidPath, child.pid.toString());
+        } catch (error) {
+            console.error('There was an error trying to save the process PID');
+            console.error(error);
+        }
+    }
+
+    process.exit();
+}
+
+main();
---
+++ bin/worker-stop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+
+program.parse(process.argv);
+
+process.exit();
